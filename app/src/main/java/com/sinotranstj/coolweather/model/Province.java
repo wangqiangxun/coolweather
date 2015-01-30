b07f670d8cfc7a0b2fb941ade07df86fd688fb19
wzl <wangzhilei@sinotrans.com>
2015-01-30 11:15:59 +0800

    add new dbhelper class and table

---
+++ app/src/main/java/com/sinotranstj/coolweather/model/Province.java
@@ -0,0 +1,36 @@
+package com.sinotranstj.coolweather.model;
+
+/**
+ * Created by wzl on 2015/1/29.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
+
+
